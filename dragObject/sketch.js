6f8615792858b977836d35faf156936a3fe93573
Nguyễn Khoa <khoacode1305@gmail.com>
2024-10-30 22:28:17 -0700

    feat: create result notification and start button

--- dragObject/sketch.js
+++ dragObject/sketch.js
@@ -2,49 +2,46 @@ function setup() {
   createCanvas(windowWidth, 500);
 }
 
-let shapeY = 50;
-let shapeX = 50;
+
 const r = 40;
 const R = r * 1.25;
-const d1 = r * 2;
-const d2 = R * 2;
 
 const TIME_EACH_GAME = 5;
 let timer = TIME_EACH_GAME;
 
+let shapeY = 50;
+let shapeX = 50;
 let x2 = 455;
 let y2 = 335;
+
 let shapeMove = false;
-let completeLevel = false;
-let gameStart = true;
-var gameScore = 0;
+let gameStart = false;
+let gameScore = 0;
 
 function distance ([x1, y1], [x2, y2]) {
   return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 }
 function draw(){
   background('lightblue');
-  
+
   fill("white");
   drawingContext.setLineDash([15, 5]);
-  circle(x2, y2, d2);
+  circle(x2, y2, R * 2);
   
   fill("lightyellow");
   drawingContext.setLineDash([0, 0]);
-  let circle1 = circle(shapeX, shapeY, d1);
+  circle(shapeX, shapeY, r * 2);
   
-  if (frameCount % 60 == 0 && timer > 0) {
-    timer--;
-    time.textContent = `Timer: ${timer}`
+  if (gameStart) {
+    if (frameCount % 60 == 0 && timer > 0) {
+      timer--;
+      time.textContent = `Timer: ${timer}`;
+    }
+    else if (timer == 0) {
+      gameStart = false;
+      finishGame();
+    }
   }
-  if (timer == 0) {
-    gameStart = false;
-    textSize(35);
-    textStyle(BOLD);
-    text("GAME OVER", 250, 275);
-    text("SCORE: " + gameScore, 250, 310);
-  }
-  
 }
 
 //check if mouse in within circle
@@ -65,13 +62,11 @@ function mouseReleased(){
     result.textContent = "Good";
     gameScore++
     score.textContent = `Score: ${gameScore}`;
-    completeLevel = true;
     x2 = random(100, windowWidth - 200);
     y2 = random(100, 400);
   }
   else if (gameStart == true) {
     result.textContent = "Bad";
-    completeLevel = false;
   }
   else {
     result.textContent = "Result"
@@ -80,7 +75,7 @@ function mouseReleased(){
 
 //allows object to move with user input
 function mouseDragged(){
-  if (shapeMove){
+  if (shapeMove && gameStart){
     shapeY = mouseY;
     shapeX = mouseX;
   }
@@ -90,3 +85,24 @@ function mouseDragged(){
 const result = document.getElementById("result");
 const time = document.getElementById("timer");
 const score = document.getElementById("score");
+const startButton = document.getElementById("start");
+const currentScore = document.getElementById("currentscore");
+const finish = document.getElementById("finish");
+
+function intialize() {
+  timer = TIME_EACH_GAME;
+  gameScore = 0;
+  gameStart = true;
+}
+startButton.addEventListener("click", () => {
+  startButton.style.visibility = "hidden";
+  finish.style.visibility = "hidden";
+  intialize();
+});
+
+function finishGame() {
+  finish.style.visibility = "visible";
+  startButton.style.visibility = "visible";
+  startButton.innerHTML = "<p>Try Again</p>";
+  currentScore.textContent = `Score: ${gameScore}`;
+}
